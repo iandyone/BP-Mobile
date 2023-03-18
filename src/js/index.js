512abc895bc4a56d412483cd54cab8b0ef7eb27d
Andrei Bernatski <iandy1731@gmail.com>
2023-03-18 18:24:08 +0300

    feat: add rate changing

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // import '@scss/style.scss';
 import '@css/style.css';
+
+const rates = document.querySelector('.rates');
+const ratesItems = document.querySelectorAll('.rates__item');
+const mainButton = document.querySelector(".main__button");
+
+
+/* === CHOOSE THE RATE === */
+rates.addEventListener('click', (e) => {
+    const rate = e.target.closest('.rates__item');
+    if (rate) {
+        ratesItems.forEach(item => item.classList.add('blurred'));
+        rate.classList.remove('blurred');
+        mainButton.href = rate.dataset.link;
+    }
+})
